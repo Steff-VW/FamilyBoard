50920023a5f8d81897e9b80dfd5eb3b2efef182c
Steff-VW <steffvw@gmail.com>
2025-04-04 17:49:38 +0200

    added header and index + basic folder setup Time to learn tailwind!

---
+++ family-board/src/components/header/header.tsx
@@ -0,0 +1,15 @@
+import Nav from "../nav/nav";
+
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <h1>Family Board</h1>
+                <Nav />
+            </header>
+        </div>
+    )
+}
+
+export default Header;
---
+++ family-board/src/components/nav/nav.tsx
@@ -0,0 +1,23 @@
+
+
+const Nav = () => {
+    return(
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/about">About</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Nav;
